4ec7f1e621282320a5477c12a260c7956b894318
abhisheksingh23 <knowabhi23@gmail.com>
2016-03-08 18:26:37 +0530

    The file content in Json decoded into Go Struct

--- mockup.go
+++ mockup.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"log"
 )
 
 var mockupResponse string
 
+type Location struct {
+	Lat float64 `json:"latitude"`
+	Long float64 `json:"longitude"`
+	Turn int64 	`json:"turn"`
+}
+
+type MockDataType struct {
+	Name string `json:"algorithm"`
+	BatteryStatus float64 `json:"batteryStatus"`
+	Locations []Location 	`json:"route"`
+	Time float64 	`json:"time"`
+	Distance float64 `json:"distance"`
+}
+
 func init() {
 	fileContent, _ := ioutil.ReadFile("mockup-response.json")
+	var data MockDataType
+	err := json.Unmarshal(fileContent, &data)
+    if err != nil {
+        log.Fatal(err)
+    }
 	mockupResponse = string(fileContent)
 }
 
